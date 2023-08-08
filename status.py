7b3d3f35393f27fd20957802978393a7a25b946b
mgismissing <gabrycraft2010@gmail.com>
2023-08-08 12:30:52 +0200

    tested "status"

--- status.py
+++ status.py
@@ -1,14 +1,10 @@
 import colorama
-colorama.init()
+colorama.init(autoreset=True)
 def info(code: int = 0, desc: str = "No description", color: bool = True, div: str = " ", end: str = "\n"):
-	if color: print(end=colorama.Fore.CYAN)
-	print(f"{code}{div}{desc}", end=end)
+	print(f"{colorama.Fore.CYAN * color}{code}{div}{desc}", end=end)
 def warn(code: int = 0, desc: str = "No description", color: bool = True, div: str = " ", end: str = "\n"):
-	if color: print(end=colorama.Fore.YELLOW)
-	print(f"{code}{div}{desc}", end=end)
+	print(f"{colorama.Fore.YELLOW * color}{code}{div}{desc}", end=end)
 def err(code: int = 0, desc: str = "No description", color: bool = True, div: str = " ", end: str = "\n"):
-	if color: print(end=colorama.Fore.RED)
-	print(f"{code}{div}{desc}", end=end)
+	print(f"{colorama.Fore.RED * color}{code}{div}{desc}", end=end)
 def fatal(code: int = 0, desc: str = "No description", color: bool = True, div: str = " ", end: str = "\n"):
-	if color: print(end=colorama.Fore.BLACK+colorama.Back.RED)
-	print(f"{code}{div}{desc}", end=end)
+	print(f"{(colorama.Fore.WHITE + colorama.Back.RED) * color}{code}{div}{desc}", end=end)
